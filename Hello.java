4ef1da15e5e5e483a4a1a3198672fff40826a02b
Goutham Gollapalli <124399318+GG167@users.noreply.github.com>
2025-04-22 07:55:07 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class Hello{
-  public static void main(Srtring args[])
+  public static void main(String args[])
   {
     System.out.println("Hello world this is Devops Lab");
   }
